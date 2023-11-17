d4db2573876bc8c439b8086dff64dbe245708dc2
Salma Hussien <salmaa.hussien1@gmail.com>
2023-11-17 07:51:59 +0200

    fix[backend]: Add the required privileges for the user to migrate

--- dev_setup_mysql.sql
+++ dev_setup_mysql.sql
@@ -4,6 +4,7 @@ CREATE DATABASE IF NOT EXISTS `FamilyHub`;
 CREATE USER IF NOT EXISTS 'SKY'@'localhost' IDENTIFIED BY 'SKY_pwd';
 
 GRANT ALL PRIVILEGES ON `FamilyHub`.* TO 'SKY'@'localhost';
+GRANT CREATE, ALTER, DROP, REFERENCES ON *.* TO 'SKY'@'localhost';
 
 GRANT SELECT ON `performance_schema`.* TO 'SKY'@'localhost';
 
