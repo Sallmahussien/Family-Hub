fddf761651d5e2bab53c95ce20e9883e90b08c7d
Salma Hussien <salmaa.hussien1@gmail.com>
2023-11-19 05:56:54 +0200

    feat[backend]: Implement `DAO` for events `prisma` model

---
+++ model/dao/event.dao.js
@@ -0,0 +1,50 @@
+const { prisma } = require('../client.db');
+const { FeedsDao } = require('./feed.dao');
+const Feed = new FeedsDao();
+
+class EventsDao {
+
+    async createEvent (createEventDto, createFeedDTO) {
+        const feed = await Feed.createFeed(createFeedDTO)
+        createEventDto.feedId = feed.id
+        const event = await prisma.events.create({
+            data: createEventDto
+        });
+
+        return event;
+    }
+
+    async getEventsByCircleId (circleId) {
+        const feeds = await Feed.getFeedsBytype(circleId, 'EVENT');
+
+        return feeds;
+    }
+
+    async getEventById (eventId) {
+        const event = await prisma.feeds.findUnique({
+            where: {
+                id: eventId,
+                deleted: false
+            },
+            include: {
+                event: true,
+                comments: true,
+                likes: true
+            }
+        });
+
+        return event;
+    }
+
+    async updateEventById (eventId, updateEventDTO) {
+        await prisma.events.update({
+            where: {
+                id: eventId,
+            },
+            data: updateEventDTO
+        });
+    }
+
+}
+
+module.exports = { EventsDao };
