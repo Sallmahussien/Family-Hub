f22cfc22c43bb02620d469552a3619bbae66408f
Salma Hussien <salmaa.hussien1@gmail.com>
2023-11-20 07:24:38 +0200

    feat[backend]: Implement `DTO` for circles model

---
+++ model/dto/circle.dto.js
@@ -0,0 +1,11 @@
+class CirclesDto {
+    constructor (bodyRequest) {
+        this.id = bodyRequest.id;
+        this.name = bodyRequest.name;
+        this.coverPhoto = bodyRequest.coverPhoto;
+        this.deleted = bodyRequest.deleted;
+    }
+}
+
+
+module.exports = { CirclesDto };
