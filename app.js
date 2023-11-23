83e8365cf5f4bf7337524ebd83d4a0bddd8878c2
Salma Hussien <salmaa.hussien1@gmail.com>
2023-11-23 22:44:06 +0200

    feat[backend]: Implement routes for lists model

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require('express');
 
-const { router: userRouter  } = require('./routes/user.routes');
-const { router: postsRouter } = require('./routes/post.routes')
-const { router: feedsRouter } = require('./routes/feed.routes')
+const { router: usersRouter  } = require('./routes/user.routes');
+const { router: postsRouter } = require('./routes/post.routes');
+const { router: feedsRouter } = require('./routes/feed.routes');
+const { router: listsRouter } = require('./routes/list.routes');
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/v1/circles', feedsRouter);
+app.use('/api/v1/circles', listsRouter);
 app.use('/api/v1/circles', postsRouter);
-app.use('/api/v1/circles', userRouter);
+app.use('/api/v1/circles', usersRouter);
 
 
-const port = 50000;
+const port = 5000;
 app.listen(port, () => {
     console.log(`server is running on port ${port}`);
 });
---
+++ routes/list.routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const { ListsController } = require('../controller/list.controller')
+
+const router = express.Router();
+
+router.route('/:circleId/users/:userId/lists')
+    .post(ListsController.createList);
+
+router.route('/:circleId/lists')
+    .get(ListsController.getListsByCircleId);
+
+router.route('/:circleId/users/:userId/feeds/:feedId/lists/:listId')
+    .get(ListsController.getListById)
+    .put(ListsController.updateListById);
+
+module.exports = { router };
