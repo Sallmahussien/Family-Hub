d16a44d6bcba4e387250793e386c7567c310c781
Salma Hussien <salmaa.hussien1@gmail.com>
2023-11-23 22:47:07 +0200

    refactore[backend]: Implement method to validate FeedId

--- model/dao/list.dao.js
+++ model/dao/list.dao.js
@@ -1,10 +1,17 @@
 const { prisma } = require('../client.db');
 const { createFeed } = require('./common/createFeed');
 const { getFeedsBytype } = require('./common/getFeedsByType');
+const { validateCircleId } = require('./common/validateCircleId');
+const { validateUserId } = require('./common/validateUserId');
+const { validateFeedId } = require('./common/validateFeedId');
+const { validateListId } = require('./common/validateListId');
 
 class ListsDao {
 
     async createList(listDto, feedDto) {
+        await validateCircleId(feedDto.circleId);
+        await validateUserId(feedDto.circleId, feedDto.userId);
+
         feedDto.type = 'LIST';
         const feed = await createFeed(feedDto);
         listDto.feedId = feed.id;
@@ -17,13 +24,20 @@ class ListsDao {
 
 
     async getListsByCircleId(feedDto) {
+        await validateCircleId(feedDto.circleId);
+
         const feeds = await getFeedsBytype({ circleId: feedDto.circleId, type: 'LIST' });
 
         return feeds;
     }
 
 
-    async getListById(listDto) {
+    async getListById(listDto, feedDto) {
+        await validateCircleId(feedDto.circleId);
+        await validateUserId(feedDto.circleId, feedDto.userId);
+        await validateFeedId(feedDto);
+        await validateListId(listDto);
+
         const list = await prisma.lists.findUnique({
             where: {
                 id: listDto.id
@@ -33,7 +47,12 @@ class ListsDao {
         return list;
     }
 
-    async updateListById(listDto) {
+    async updateListById(listDto, feedDto) {
+        await validateCircleId(feedDto.circleId);
+        await validateUserId(feedDto.circleId, feedDto.userId);
+        await validateFeedId(feedDto);
+        await validateListId(listDto);
+
         await prisma.lists.update({
             where: {
                 id: listDto.id
