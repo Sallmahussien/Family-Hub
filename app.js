e108e408b5e45511ac808a7af4b20f1ad896f0ad
Salma Hussien <salmaa.hussien1@gmail.com>
2023-11-24 02:57:35 +0200

    feat[backend]: Implement routes for list items entity

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const { router: usersRouter  } = require('./routes/user.routes');
 const { router: postsRouter } = require('./routes/post.routes');
 const { router: feedsRouter } = require('./routes/feed.routes');
 const { router: listsRouter } = require('./routes/list.routes');
+const { router: listItemsRouter } = require('./routes/listitem.routes');
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 
 app.use('/api/v1/circles', feedsRouter);
 app.use('/api/v1/circles', listsRouter);
+app.use('/api/v1/circles', listItemsRouter);
 app.use('/api/v1/circles', postsRouter);
 app.use('/api/v1/circles', usersRouter);
 
---
+++ routes/listitem.routes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const { ListItemsController } = require('../controller/listitem.controller')
+
+const router = express.Router();
+
+router.route('/:circleId/users/:userId/feeds/:feedId/lists/:listId/listItems')
+    .post(ListItemsController.createListItem)
+    .get(ListItemsController.getListItemsByListId);
+
+router.route('/:circleId/users/:userId/feeds/:feedId/lists/:listId/listItems/:listItemId')
+    .put(ListItemsController.updateListItemById)
+    .delete(ListItemsController.deleteListItemById);
+
+module.exports = { router };
