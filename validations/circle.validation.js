983b54e71894f61b341160b30a202f6ee1547e3b
Salma Hussien <salmaa.hussien1@gmail.com>
2023-11-25 00:57:59 +0200

    feat[backend]: Implement validation layer for circles entity using `JOI`

---
+++ validations/circle.validation.js
@@ -0,0 +1,31 @@
+const joi = require('joi');
+
+class CircleValidator {
+
+    static createCircle(circleDto) {
+        const schema = joi.object({
+            id: joi.string(),
+            name: joi.string().trim().min(3).required(),
+            coverPhoto: joi.string().trim().min(3),
+            createdAt: joi.date(),
+            deleted: joi.boolean().default(false),
+        });
+
+        return schema.validate(circleDto);
+    }
+
+    static updateCircle(circleDto) {
+        const schema = joi.object({
+            id: joi.string().required(),
+            name: joi.string().trim().min(3),
+            coverPhoto: joi.string().trim().min(3),
+            createdAt: joi.date(),
+            deleted: joi.boolean().default(false),
+        });
+
+        return schema.validate(circleDto);
+    }
+
+}
+
+module.exports = { CircleValidator };
