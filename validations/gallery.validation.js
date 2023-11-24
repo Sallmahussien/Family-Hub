44330a1224c0b8b61c1c0452fd09e95fe9c6f08c
Salma Hussien <salmaa.hussien1@gmail.com>
2023-11-24 07:13:31 +0200

    feat[backend]: Implement validation layer for gallery entity using `JOI`

---
+++ validations/gallery.validation.js
@@ -0,0 +1,27 @@
+const joi = require('joi');
+
+class GalleryValidator {
+    static createGallery(galleryDto) {
+        const schema = joi.object({
+            id: joi.string(),
+            feedId: joi.string(),
+            caption: joi.string().min(2),
+            photo: joi.string().trim().min(2),
+        });
+
+        return schema.validate(galleryDto);
+    }
+
+    static updateGallery(galleryDto) {
+        const schema = joi.object({
+            id: joi.string().required(),
+            feedId: joi.string().required(),
+            caption: joi.string().min(2),
+            photo: joi.string().trim().min(2),
+        });
+
+        return schema.validate(galleryDto);
+    }
+}
+
+module.exports = { GalleryValidator };
