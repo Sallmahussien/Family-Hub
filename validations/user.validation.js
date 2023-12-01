9160b230a67109bd4ccba83ee1c55692eb8f31b2
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-12-01 06:06:34 +0200

    feat[backend]: Add validation for user login

--- validations/user.validation.js
+++ validations/user.validation.js
@@ -20,6 +20,24 @@ class UsersValidator {
         return schema.validate(userDto);
     }
 
+    static login(userDto) {
+        const schema = joi.object({
+            id: joi.string(),
+            circleId: joi.string(),
+            firstName: joi.string().trim().min(3),
+            lastName: joi.string().trim().min(3),
+            role: joi.string().valid('CREATOR', 'MEMEBER').default('MEMEBER'),
+            email: joi.string().email().required(),
+            password: joi.string().min(6).required(),
+            profilePhoto: joi.string().trim(),
+            birthdate: joi.date().iso(),
+            position: joi.string().valid('FATHER', 'MOTHER', 'SON', 'DAUGHTER'),
+            deleted: joi.boolean(),
+        });
+
+        return schema.validate(userDto);
+    }
+
     static updateUser(userDto) {
         const schema = joi.object({
             id: joi.string().required(),
