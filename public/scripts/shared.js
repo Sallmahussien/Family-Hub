dde2f102c2a07be4a9922c8408f98182927d28c9
kholoudxs55kh <kfattem@gmail.com>
2023-12-18 01:53:21 +0200

    feat[api]: Implement shared file with methods to unit the circlename, username, profilePhoto and circleCover

---
+++ public/scripts/shared.js
@@ -0,0 +1,39 @@
+const getElement = (id) => document.getElementById(id);
+
+const userDataFromSession = sessionStorage.getItem('userData');
+const userData = JSON.parse(userDataFromSession);
+// console.log(userData);
+const userId = userData.id;
+const circleId = userData.circleId;
+const userEmail = userData.email;
+const userFirstName = userData.firstName;
+const userProfile = userData.profilePhoto;
+
+
+document.getElementById('userFName').textContent = userFirstName;
+
+document.getElementById('userMainPhoto').src = `../../images/${userProfile}`;
+
+
+async function getCircleName() {
+    const response = await fetch(`/api/v1/circles/${circleId}`);
+    const data = await response.json();
+
+    const circleName = data.name;
+
+    document.getElementById('circleName').textContent = circleName;
+    return circleName;
+}
+
+getCircleName();
+
+
+async function getCircleCover() {
+    const response = await fetch(`/api/v1/circles/${circleId}`);
+    const data = await response.json();
+    const circleCover = data.coverPhoto;
+    document.getElementById('circleCoverPic').src = `../../images/${circleCover}`? `../../images/${circleCover}` : `/imgs/cover.png`;
+    return circleCover;
+}
+
+getCircleCover();
