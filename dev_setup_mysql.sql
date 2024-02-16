da5e08ce3b6da735e55d1e42a5aa533aceba53b9
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-02-16 17:16:25 +0200

    fix[backend]: Grant index to user SKY

--- dev_setup_mysql.sql
+++ dev_setup_mysql.sql
@@ -5,6 +5,7 @@ CREATE USER IF NOT EXISTS 'SKY'@'localhost' IDENTIFIED BY 'SKY_pwd';
 
 GRANT ALL PRIVILEGES ON `FamilyHub`.* TO 'SKY'@'localhost';
 GRANT CREATE, ALTER, DROP, REFERENCES ON *.* TO 'SKY'@'localhost';
+GRANT INDEX ON `FamilyHub`.* TO 'SKY'@'localhost';
 
 GRANT SELECT ON `performance_schema`.* TO 'SKY'@'localhost';
 
